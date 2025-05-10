551b13ea8f4fc8642e6a68d182a36e5b10e9fc75
Kesmish <kesmish@gmail.com>
2025-05-09 23:52:30 -0400

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Kesmish",                         # Tu nombre
+    author_email="kesmish@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/kesmish/tercerrepo",     # URL del proyecto
+)
